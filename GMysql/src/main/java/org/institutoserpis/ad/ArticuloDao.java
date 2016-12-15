fd08775f1d1131754a565fb570c86e88f8e8c117
jonathanortizdam1993445 <dam.jortiz@institutoserpis.org>
2016-12-15 09:23:35 +0100

    mostrar datos en columna

--- GMysql/src/main/java/org/institutoserpis/ad/ArticuloDao.java
+++ GMysql/src/main/java/org/institutoserpis/ad/ArticuloDao.java
@@ -2,6 +2,7 @@ package org.institutoserpis.ad;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,17 +15,26 @@ public class ArticuloDao {
 				"jdbc:mysql://localhost/dbprueba", 
 				"root", 
 				"sistemas");
-		Statement statement=connection.createStatement();
-		ResultSet resultSet= statement.executeQuery("SELECT * FROM articulo");
+		PreparedStatement ps= connection.prepareStatement("select * from articulo where id>?");
+		
+		ps.setObject(1, Long.parseLong(args[0]));
+		ResultSet resultSet=ps.executeQuery();
+		
+		//Statement statement=connection.createStatement();
+		//ResultSet resultSet= statement.executeQuery("SELECT * FROM articulo");
 		
 		System.out.printf("%5s %-30s %10s %5s\n", "id", "nombre", "precio","categoria");
 		while (resultSet.next()) {
 			
-			System.out.printf("%5d %-30s %10s %5d\n",resultSet.getObject("id"),resultSet.getObject("nombre"),resultSet.getObject("precio"), resultSet.getObject("categoria"));
+			System.out.printf("%5d %-30s %10s %5d\n",
+					resultSet.getObject("id"),
+					resultSet.getObject("nombre"),
+					resultSet.getObject("precio"), 
+					resultSet.getObject("categoria"));
 			
 		}
 		
-		statement.close();
+		ps.close();
 			connection.close();	
 			System.out.println("fin");
 
